86063c5c393962fbf819786a0e8a39b8dfc8695e
amazing-stack <amazingowusu540@gmail.com>
2020-07-15 20:03:25 +0000

    Solve firebase sign in/ sighn up

---
+++ app/src/main/java/com/example/welcome/Cloud_firestoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cloud_firestoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cloud_firestore);
+    }
+}
